ff9c281fe6e852d2482abbf4389893d4d8f3c71a
karamov-git <ybiz177@gmail.com>
2020-09-03 21:59:58 +0500

    init proj

---
+++ ReactionSlackBotRunner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReactionSlackBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm Reaction Slack bot");
+        }
+    }
+}
